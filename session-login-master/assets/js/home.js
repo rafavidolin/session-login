3e643e9384dd3b43212870b8fbeeb50a66919ae8
Rafael Vidolin <rafavidolin@hotmail.com>
2018-08-30 22:10:29 +0000

    removido pasta errada

--- session-login-master/assets/js/home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    var $login = $("#login");
-    var user = ManageUsers.logged();
-    $login.html(user.name);
-
-    $("#logout").click(ManageUsers.logout);
-});
--- session-login-master/assets/js/login/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    var $email = $("#email"),
-        $password = $("#password");
-
-    $("form").submit(function (ev) {
-        if (!ManageUsers.login($email.val(), $password.val()))
-            ev.preventDefault();
-    });
-});
